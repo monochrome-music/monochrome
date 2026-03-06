b33830cb8be8c48ec6255aacede6901f14a71056
edideaur <eduard@prigoana.com>
2026-03-07 02:57:28 +0300

    lets try that again

--- js/accounts/config.js
+++ js/accounts/config.js
@@ -1,14 +1,18 @@
-// js/accounts/config.js
 import { Client, Account } from 'appwrite';
 
-const client = new Client().setEndpoint('https://auth.samidy.xyz/v1').setProject('auth-for-monochrome');
+const getEndpoint = () => {
+    const hostname = window.location.hostname;
+    if (hostname.endsWith('monochrome.tf') || hostname === 'monochrome.tf') {
+        return 'https://auth.monochrome.tf/v1';
+    }
+    return 'https://auth.samidy.com/v1';
+};
+
+const client = new Client()
+    .setEndpoint(getEndpoint())
+    .setProject('auth-for-monochrome');
 
 const account = new Account(client);
-
 export { client, account as auth };
-export const saveFirebaseConfig = () => {
-    console.log('ill fix this tomorrow');
-};
-export const clearFirebaseConfig = () => {
-    console.log('ill fix this tomorrow');
-};
+export const saveFirebaseConfig = () => { console.log("ill fix this tomorrow"); };
+export const clearFirebaseConfig = () => { console.log("ill fix this tomorrow"); };
