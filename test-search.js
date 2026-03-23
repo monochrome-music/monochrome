2440939f4ba1ef184c881b732eea52e2e410504d
Daniel <790119+DanTheMan827@users.noreply.github.com>
2026-03-23 13:42:03 -0500

    refactor(hifi): update HiFi.ts to add trackManifests route and refactor

--- test-search.js
+++ test-search.js
@@ -1,7 +1,7 @@
 import { HiFiClient } from './js/HiFi.ts';
 async function test() {
     const client = new HiFiClient();
-    const res = await client.queryResponse('/search/?q=alskdjfalksjdfld&limit=5');
+    const res = await client.query('/search/?q=alskdjfalksjdfld&limit=5');
     const json = await res.json();
     console.log(JSON.stringify(json.data || {}));
 }
