859beb05571150c12590adc9d28c9fbd970cc7d6
ahbh <mtsbh@proton.me>
2026-03-26 11:38:20 +0200

    test android auto

--- android/app/src/main/java/tf/monochrome/music/MainActivity.java
+++ android/app/src/main/java/tf/monochrome/music/MainActivity.java
@@ -1,5 +1,20 @@
 package tf.monochrome.music;
 
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        Intent serviceIntent = new Intent(this, MediaPlaybackService.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(serviceIntent);
+        } else {
+            startService(serviceIntent);
+        }
+    }
+}
