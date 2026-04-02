107706a6e571ce2f335d37d68d550aff8c6c29c8
edideaur <eduard@prigoana.com>
2026-04-02 10:35:08 +0000

    ok FINE

--- functions/unreleased/index.js
+++ functions/unreleased/index.js
@@ -14,14 +14,12 @@ export async function onRequest(context) {
             <meta property="og:site_name" content="Monochrome">
             <meta property="og:title" content="Monochrome Music | Unreleased">
             <meta property="og:description" content="Stream unreleased music on Monochrome. Provided by Artistgrid.">
-            <meta property="og:image" content="https://monochrome.tf/assets/appicon.png">
             <meta property="og:type" content="website">
             <meta property="og:url" content="${pageUrl}">
 
-            <meta name="twitter:card" content="summary_large_image">
+            <meta name="twitter:card" content="summary">
             <meta name="twitter:title" content="Monochrome Music | Unreleased">
             <meta name="twitter:description" content="Stream unreleased music on Monochrome. Provided by Artistgrid.">
-            <meta name="twitter:image" content="https://monochrome.tf/assets/appicon.png">
         </head>
         <body>
             <h1>Monochrome Music | Unreleased</h1>
