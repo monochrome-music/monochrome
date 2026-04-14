19cecc0c762b40f18ed6256efc8da285ff35d544
edideaur <182119792+edideaur@users.noreply.github.com>
2026-04-14 19:46:46 +0000

    style: auto-fix linting issues

--- functions/album/[id].js
+++ functions/album/[id].js
@@ -134,7 +134,20 @@ class ServerAPI {
     }
 }
 
-const _cr = ['emVl','em1j','emVlIG11c2lj','emVlIGVudGVydGFpbm1lbnQ=','emVlbA==','Ym9sbHl3b29kIG11c2ljIGluZGlh','emVlIHJlY29yZHM=','emluZyBtdXNpYw==','ZXRjIGJvbGx5d29vZA==','emVlIHN0dWRpb3M=','emluZGFnaSBtdXNpYw==','emVlNQ=='].map(atob);
+const _cr = [
+    'emVl',
+    'em1j',
+    'emVlIG11c2lj',
+    'emVlIGVudGVydGFpbm1lbnQ=',
+    'emVlbA==',
+    'Ym9sbHl3b29kIG11c2ljIGluZGlh',
+    'emVlIHJlY29yZHM=',
+    'emluZyBtdXNpYw==',
+    'ZXRjIGJvbGx5d29vZA==',
+    'emVlIHN0dWRpb3M=',
+    'emluZGFnaSBtdXNpYw==',
+    'emVlNQ==',
+].map(atob);
 const _isBlockedCopyright = (c) => !!c && _cr.some((s) => c.toLowerCase().includes(s));
 
 export async function onRequest(context) {
