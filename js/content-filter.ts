04be94552d20c4c04e573fcafa9232fbf57ee4af
edideaur <eduard@prigoana.com>
2026-04-14 23:03:13 +0300

    ouuu chiild things are gonna get easier

--- js/content-filter.ts
+++ js/content-filter.ts
@@ -1,11 +1,11 @@
 const _cr = [
-    'emVl', // zee
-    'em1j', // zmc
-    'emluZyBtdXNpYw==', // zing music
-    'ZXRjIGJvbGx5d29vZA==', // etc bollywood
-    'Ym9sbHl3b29kIG11c2lj', // bollywood music
-    'ZXNzZWw=', // essel
-    'emluZGFnaQ==', // zindagi
+    'emVl',     
+    'em1j',
+    'emluZyBtdXNpYw==', 
+    'ZXRjIGJvbGx5d29vZA==',
+    'Ym9sbHl3b29kIG11c2lj',  
+    'ZXNzZWw=',    
+    'emluZGFnaQ==',
 ].map(atob);
 
 export const isBlockedCopyright = (c: string | null | undefined): boolean =>
