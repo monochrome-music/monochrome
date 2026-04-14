0d3809ca3d8faca902fe2fa75c1508b6375678e0
edideaur <eduard@prigoana.com>
2026-04-14 23:02:32 +0300

    ouuuu

--- functions/album/[id].js
+++ functions/album/[id].js
@@ -135,20 +135,13 @@ class ServerAPI {
 }
 
 const _cr = [
-    'emVl',
-    'em1j',
-    'emVlIG11c2lj',
-    'emVlIGVudGVydGFpbm1lbnQ=',
-    'emVlbA==',
-    'Ym9sbHl3b29kIG11c2ljIGluZGlh',
-    'emVlIHJlY29yZHM=',
-    'emluZyBtdXNpYw==',
-    'ZXRjIGJvbGx5d29vZA==',
-    'emVlIHN0dWRpb3M=',
-    'emluZGFnaSBtdXNpYw==',
-    'emVlNQ==',
-    'Ym9sbHl3b29kIG11c2lj',
-    'ZXNzZWw=',
+    'emVl', // zee
+    'em1j', // zmc
+    'emluZyBtdXNpYw==', // zing music
+    'ZXRjIGJvbGx5d29vZA==', // etc bollywood
+    'Ym9sbHl3b29kIG11c2lj', // bollywood music
+    'ZXNzZWw=', // essel
+    'emluZGFnaQ==', // zindagi
 ].map(atob);
 const _isBlockedCopyright = (c) => !!c && _cr.some((s) => c.toLowerCase().includes(s));
 
