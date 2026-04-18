067b68c4c695eb1c016911e612521a8f6782cc63
uimaxbai <61615730+uimaxbai@users.noreply.github.com>
2026-04-18 14:12:11 +0100

    fix proxy again

--- js/proxy-utils.js
+++ js/proxy-utils.js
@@ -1,4 +1,4 @@
 export const getProxyUrl = (url) => {
     if (window.__tidalOriginExtension) return url;
-    return `https://audio-proxy.binimum.org/proxy-audio?url=${encodeURIComponent(url)}`;
+    return `https://audio-proxy.binimum.org/proxy-audio?url=${url}`;
 };
