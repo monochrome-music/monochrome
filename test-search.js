d4f6c5911fbee00b7e8c9dc98a1c851886fdf6e0
uimaxbai <61615730+uimaxbai@users.noreply.github.com>
2026-04-18 10:02:34 +0100

    Revert "style: auto-fix linting issues"
    This reverts commit 4b6f81b2ec424ceb29db065667180f495af01998.

--- test-search.js
+++ test-search.js
@@ -4,13 +4,7 @@ import { LosslessAPI } from './js/api.js';
 // mock out modules to make LosslessAPI load in bun
 import { mock } from 'bun:test';
 mock.module('./js/icons.ts', () => ({}));
-mock.module('./js/settings.js', () => ({
-    devModeSettings: { isEnabled: () => false },
-    syncManager: {},
-    musicProviderSettings: {},
-    audioSettings: {},
-    apiSettings: {},
-}));
+mock.module('./js/settings.js', () => ({ devModeSettings: { isEnabled: () => false }, syncManager: {}, musicProviderSettings: {}, audioSettings: {}, apiSettings: {} }));
 
 globalThis.localStorage = { getItem: () => null, setItem: () => {}, removeItem: () => {} };
 globalThis.window = { matchMedia: () => ({ matches: false }) };
@@ -22,12 +16,12 @@ async function test() {
     // mock cache
     api.cache = { get: () => null, set: () => {} };
 
-    api.fetchWithRetry = async function (relativePath, options) {
-        console.log('fetchWithRetry called:', relativePath);
+    api.fetchWithRetry = async function(relativePath, options) {
+        console.log("fetchWithRetry called:", relativePath);
         return HiFiClient.instance.query(relativePath);
     };
 
     const res = await api.search('coldplay');
-    console.log('Returned tracks:', res.tracks?.items?.length);
+    console.log("Returned tracks:", res.tracks?.items?.length);
 }
 test().catch(console.error);
