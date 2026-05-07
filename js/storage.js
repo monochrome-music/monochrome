cd8c22b1dc96930549acdb9fade1b410ddc6e8ba
edideaur <eduard@prigoana.com>
2026-05-07 08:23:59 +0300

    oops

--- js/storage.js
+++ js/storage.js
@@ -97,7 +97,7 @@ export const apiSettings = {
                         { url: 'https://wolf.qqdl.site', version: '2.6' },
                     ],
                     qobuz: [
-                        { url: 'https://trypt-hifi-dl-456461932686.us-west1.run.app', version: '1.0' }
+                        { url: 'https://trypt-hifi-dl-456461932686.us-west1.run.app', version: '1.0' },
                         { url: 'https://qobuz.kennyy.com.br', version: '1.0' },
                     ],
                 };
