7f25c94cc4a2d238aa22c65ef30e29bd2f0ebe5b
edideaur <eduard@prigoana.com>
2026-05-07 08:22:39 +0300

    ok NOW PLEASE LET THIS FIX

--- js/storage.js
+++ js/storage.js
@@ -97,8 +97,8 @@ export const apiSettings = {
                         { url: 'https://wolf.qqdl.site', version: '2.6' },
                     ],
                     qobuz: [
+                        { url: 'https://trypt-hifi-dl-456461932686.us-west1.run.app', version: '1.0' }
                         { url: 'https://qobuz.kennyy.com.br', version: '1.0' },
-                        { url: 'https://trypt-hifi-dl-456461932686.us-west1.run.app', version: '1.0' },
                     ],
                 };
                 this.instancesLoaded = true;
@@ -129,7 +129,7 @@ export const apiSettings = {
 
             // Ensure default Qobuz instance is always available
             if (groupedInstances.qobuz.length === 0) {
-                groupedInstances.qobuz = [{ url: 'https://qobuz.kennyy.com.br', version: '1.0' }];
+                groupedInstances.qobuz = [{ url: 'https://trypt-hifi-dl-456461932686.us-west1.run.app', version: '1.0' }];
             }
 
             this.defaultInstances = groupedInstances;
