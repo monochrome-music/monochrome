0a1e3ebca18a25465b472b21fad19ced298f7a09
Samidy <samidy@proton.me>
2026-07-31 19:34:07 +0300

    WTF AM I DOING IM SO FUCKING STUPID WHY DID I THINK THAT WOULD WORK U CANT BLAME ME I WAS PANIC FIXING

--- js/app.js
+++ js/app.js
@@ -344,18 +344,6 @@ function initializeKeyboardShortcuts(player, _audioPlayer) {
 }
 
 
-async function openDiscord(link) {
-    let discordbtn = document.querySelector(".discordbtn");
-
-
-    discordbtn.addEventListener(click, () => {
-        window.open(link, '_blank');
-    });
-}
-
-
-openDiscord("https://discord.gg/4DYm4artsN")
-
 async function closeFullscreenOverlay() {
     if (UIRenderer.instance?.dismissFullscreenCover) {
         await UIRenderer.instance.dismissFullscreenCover({ animate: false });
