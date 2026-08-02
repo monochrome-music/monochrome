2edcded7e167060dbff33d1751e4d1feebbe8415
evnmcnb <evnmcnb@gmail.com>
2026-08-02 06:49:03 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -66,14 +66,14 @@ export default defineConfig(({ mode }) => {
         server: {
             fs: {
                 allow: ['.', 'node_modules'],
-                // host: true,
-                // allowedHosts: ['<your_tailscale_hostname>'], // e.g. pi5.tailf5f622.ts.net
+                host: true,
+                allowedHosts: ['music.dace.org.uk'], // e.g. pi5.tailf5f622.ts.net
             },
         },
-        // preview: {
-        //     host: true,
-        //     allowedHosts: ['<your_tailscale_hostname>'], // e.g. pi5.tailf5f622.ts.net
-        // },
+        preview: {
+            host: true,
+            allowedHosts: ['music.dace.org.uk'], // e.g. pi5.tailf5f622.ts.net
+        },
         build: {
             outDir: 'dist',
             emptyOutDir: true,
