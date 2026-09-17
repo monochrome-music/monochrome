75026930bae4085abd775b5e23f1c638ab7d40a4
binimum <uimax.bai@gmail.com>
2026-09-17 22:14:41 +0000

    Deploying to dist from @ monochrome-music/monochrome@f44aa69a03ea103122e4286e61d9d16a05ceac86 🚀

---
+++ assets/index-j7Pwu-qL.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-BAeYetkP.js","./index-Cl6pkaWa.js","./index-BHkV4DoU.css"])))=>i.map(i=>d[i]);
+import{ab as e,a2 as o}from"./index-Cl6pkaWa.js";const i=e("Browser",{web:()=>o(()=>import("./web-BAeYetkP.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(r=>new r.BrowserWeb)});export{i as Browser};
